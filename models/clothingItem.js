641ecd1d1a1545831cfef70b607e9a284820ee53
Aaronicle <aaroncolorguy@gmail.com>
2025-02-23 14:47:32 -0500

    finished clothingItem

---
+++ models/clothingItem.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clothingItem = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  weather: {
+    type: String,
+    required: true,
+  },
+  imageURL: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: "Link is not valid",
+    },
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  likes: {
+    type: mongoose.Types.ObjectId,
+    default: {},
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("clothingItems", clothingItem);
